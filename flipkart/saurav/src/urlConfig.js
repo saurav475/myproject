9527373c0228849e66436ea972c6135ddc1cf416
saurav475 <64941922+saurav475@users.noreply.github.com>
2021-01-25 15:25:16 +0530

    Delete flipkart/saurav directory

--- flipkart/saurav/src/urlConfig.js
+++
@@ -1,6 +0,0 @@
-const baseUrl = "https://flipkart-rest-server.herokuapp.com";
-
-export const api = `${baseUrl}/api`;
-export const generatePublicUrl = (fileName) => {
-  return `${baseUrl}/public/${fileName}`;
-};
